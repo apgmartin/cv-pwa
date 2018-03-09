c2106e37d6dad67ef80d7ced78267e462b91a362
Ali Martin <alistairmartin@live.com>
2018-03-09 17:45:12 +0000

    messing around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { FooterComponent } from './footer/footer.component';
 import { SkillsViewerComponent } from './skills-viewer/skills-viewer.component';
-import { AboutMeHomeComponent } from './about-me-home/about-me-home.component';
-import { ProjectsHomeComponent } from './projects-home/projects-home.component';
+import { AboutMeHomeComponent } from './home/about-me-home/about-me-home.component';
+import { ProjectsHomeComponent } from './home/projects-home/projects-home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  }
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeCarouselComponent,
     FooterComponent,
     SkillsViewerComponent,
     AboutMeHomeComponent,
-    ProjectsHomeComponent
+    ProjectsHomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
